30c2d2252d15ecf020d309192b9270e53beacb69
Michealsc <mcrawford@student.neumont.edu>
2021-10-20 12:10:18 -0600

    testing message

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,11 @@
+public class MyArrayList implements MyList{
+
+    public void add(T val);
+    void remove(T val);
+    ArrayList<T> get();
+    int size();
+
+    @Override
+    public String toString(String s);
+
+}
---
+++ src/main/java/MyList.java
@@ -0,0 +1,9 @@
+public interface MyList {
+
+
+    public void add(T val);
+    public void remove(T val);
+    public ArrayList<T> get();
+    public int size();
+
+}
